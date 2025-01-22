ac41d48c3e28426d5445a9fa4590221f25723c8d
MAHANANDA MAYEKAR <mayekardiksha750@gmail.com>
2025-01-22 14:42:04 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import AppRoutes from './routes/AppRoutes';
+
+const App = () => {
+  return (
+    <>
+      <div className="px-4 sm:px-[5vw] md:px-[7vw]  lg:px-[9vw]">
+        <AppRoutes />
+      </div>
+    </>
+  );
+}
+
+export default App
