2067bb3bf6e5ab3291941fbe9512a2defc176064
MAHANANDA MAYEKAR <mayekardiksha750@gmail.com>
2025-01-24 21:01:57 +0530

    bug fix

--- src/components/LatestProductItem.jsx
+++ src/components/LatestProductItem.jsx
@@ -6,7 +6,7 @@ import { Link } from 'react-router-dom';
 const LatestProductItem = ({ productId, image, name, price }) => {
     const { currency, delivery_fee } = useShopContext();
     return (
-      <Link to={`product/${productId}`}>
+      <Link to={`/product/${productId}`}>
         <div className="overflow-hidden">
           <img
             src={image[0]}
