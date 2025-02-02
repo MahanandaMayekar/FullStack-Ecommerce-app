539a976d8bf71e7ba3081d23d1c58cc77a88142d
Mahananda.Mayekar <mayekardiksha750@gmail.com>
2025-02-02 20:08:26 +0530

    initial commit

---
+++ ADMIN/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='font-semibold'>
+      hello
+    </div>
+  )
+}
+
+export default App
---
+++ ADMIN/src/pages/ProductOrders.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ProductOrders = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ProductOrders
