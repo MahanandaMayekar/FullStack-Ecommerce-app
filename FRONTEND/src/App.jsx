46e1f5c420249576eb9232898eab1eb19e98cf6e
Mahananda.Mayekar <mayekardiksha750@gmail.com>
2025-02-16 20:50:21 +0530

    adde api and hook for fetching products

--- FRONTEND/src/App.jsx
+++ FRONTEND/src/App.jsx
@@ -1,16 +1,23 @@
 import React from 'react'
 import AppRoutes from './routes/AppRoutes';
 import { AppContextProvider } from './context/AppContextProvider';
- import { ToastContainer, toast } from "react-toastify";
+import { ToastContainer } from "react-toastify";
+ import {
+   QueryClient,
+   QueryClientProvider,
+ } from "@tanstack/react-query";
 
 const App = () => {
+  const queryClient = new QueryClient();
   return (
     <>
       <div className="px-4 sm:px-[5vw] md:px-[7vw]  lg:px-[9vw]">
-        <AppContextProvider>
-          <ToastContainer />
-          <AppRoutes />
-        </AppContextProvider>
+        <QueryClientProvider client={queryClient}>
+          <AppContextProvider>
+            <ToastContainer />
+            <AppRoutes />
+          </AppContextProvider>
+        </QueryClientProvider>
       </div>
     </>
   );
