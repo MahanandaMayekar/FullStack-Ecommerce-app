450955cf3105c8f27d61c126f25ecb85463a0ab5
Mahananda.Mayekar <mayekardiksha750@gmail.com>
2025-02-22 14:34:56 +0530

    added cart routes

--- BACKEND/SRC/routes/ApiRoute.js
+++ BACKEND/SRC/routes/ApiRoute.js
@@ -1,11 +1,12 @@
 import express from 'express'
 import UserRoutes from './UserRoutes.js'
 import ProductRoutes from './ProductRoute.js'
-
+import CartRoutes from './CartRoute.js'
 const Router = express.Router()
  
 Router.use("/users", UserRoutes)
-Router.use("/products",ProductRoutes)
+Router.use("/products", ProductRoutes)
+Router.use("/cart", CartRoutes);
 
 
 export default Router
---
+++ BACKEND/SRC/routes/CartRoute.js
@@ -0,0 +1,13 @@
+import express from "express"
+import { AddProductToCartController, UpdateProducInCartController, GetUsersCartController } from '../controllers/CartController.js'
+import { AuthMiddleware } from '../middleware/AuthMiddleware.js'
+const Router = express.Router()
+
+Router.get("/get-cart-items",AuthMiddleware,GetUsersCartController)
+Router.post("/add-to-cart",AuthMiddleware,AddProductToCartController)
+Router.post("/update-cart",AuthMiddleware,UpdateProducInCartController)
+
+
+
+
+export default Router
