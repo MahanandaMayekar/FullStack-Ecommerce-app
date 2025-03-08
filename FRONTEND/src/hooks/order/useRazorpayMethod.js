ddcd38e61fd76b1d0062223dc0ed1ccf46d295c1
Mahananda.Mayekar <mayekardiksha750@gmail.com>
2025-03-08 21:50:35 +0530

    Implemented mutation for payment processing

---
+++ FRONTEND/src/hooks/order/useRazorpayMethod.js
@@ -0,0 +1,56 @@
+import { useMutation } from "@tanstack/react-query";
+import useAuth from "../auth/useAuth";
+import { placeOrderByRazorpayRequest } from "@/api/orders";
+
+
+const useRazorpayMethod = () => {
+    const { auth } = useAuth();
+   const initPay = (razorpayOrder) => {
+     const options = {
+       key: import.meta.env.VITE_RAZORPAY_KEY_ID, 
+       amount: razorpayOrder.amount,
+       currency: razorpayOrder.currency,
+       name: "Your Store",
+       description: "Order Payment",
+         order_id: razorpayOrder.id,
+       receipt:razorpayOrder.receipt,
+       handler: async (paymentResponse) => {
+         console.log("Payment Success:", paymentResponse);
+        
+       },
+       prefill: {
+         email: auth?.user?.email || "", // Prefill user email
+       },
+     };
+
+     const rzp = new window.Razorpay(options);
+     rzp.open();
+   };
+
+  const {
+    isPending,
+    isSuccess,
+    error,
+    mutateAsync: orderByRazorpayMutation,
+  } = useMutation({
+    mutationFn: (orderData) =>
+      placeOrderByRazorpayRequest({ token: auth?.token, orderData }),
+    onSuccess: (data) => {
+        console.log("Successfully placed order via razorpay", data);
+        if (data?.razorpayOrder) {
+          initPay(data.razorpayOrder); 
+        }
+    },
+    onError: (err) => {
+      console.log("failed to placed order by razorpay", err);
+    },
+  });
+  return {
+    isPending,
+    isSuccess,
+    error,
+    orderByRazorpayMutation,
+  };
+};
+
+export default useRazorpayMethod;
