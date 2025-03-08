6bdba410d8c9fdfcd2900df4d4e913b434f3a8a7
Mahananda.Mayekar <mayekardiksha750@gmail.com>
2025-03-08 21:49:25 +0530

    added routes

--- BACKEND/SRC/routes/OrderRoute.js
+++ BACKEND/SRC/routes/OrderRoute.js
@@ -1,8 +1,8 @@
 import express from "express"
 import { AdminAuthMiddleware } from '../middleware/AdminAuthMiddleware.js'
 import { AuthMiddleware } from './../middleware/AuthMiddleware.js';
-import { adminOrderListController, placeOrderCODController, placeOrderStripeController, updateStatusController, usersOrderDetailsController, verifyStripeController } from '../controllers/OrderController.js';
-
+import { adminOrderListController, placeOrderByRazorparController, placeOrderCODController, placeOrderStripeController, updateStatusController, usersOrderDetailsController, verifyStripeController } from '../controllers/OrderController.js';
+placeOrderByRazorparController
 const Router = express.Router()
 //admin
 Router.get("/list-orders", AdminAuthMiddleware, adminOrderListController);
@@ -11,7 +11,7 @@ Router.post("/status", AdminAuthMiddleware, updateStatusController);
 //payment
 Router.post("/cod", AuthMiddleware, placeOrderCODController);
 Router.post("/stripe", AuthMiddleware, placeOrderStripeController);
-Router.post("/razorPay", AuthMiddleware);
+Router.post("/razorPay", AuthMiddleware, placeOrderByRazorparController);
 //user
 Router.get("/users-orders", AuthMiddleware, usersOrderDetailsController);
 //verify payment
